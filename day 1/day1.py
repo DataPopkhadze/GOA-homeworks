4eca7a59db640b4aa15a7570a2c270af674b7a85
qweqwe1qq <dfofxadze2010@gmail.com>
2024-02-09 01:08:38 +0400

    first commit

---
+++ day 1/day1.py
@@ -0,0 +1,10 @@
+
+name = "my name is data"
+surname = "my surname is fofxadze"
+school = "i study at 175th public school"
+secondary_school = "my secondary school is GOA"
+sport = "i play basketball"
+height = "my height is 156cm"
+age = "i am 13 years old"
+print (name + " " + surname + " " + age + " " + height + " " + school + " " + secondary_school + " " + sport)
+
---
+++ day 2/day2.py
@@ -0,0 +1,6 @@
+subscribed = "i subscribed to GOA's channel and low budget stories "
+met = "sandro zabakhidze"
+GOA = "GOA is the best accademy ever, they dont only teach programming, but how to be a man too. they teach about life and thats what i like about Goal Oriented Academy."
+
+
+print (subscribed + met + GOA)
