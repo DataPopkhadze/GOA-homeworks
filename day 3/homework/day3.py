83ec6eeac632c276ebbe35970d5fec43f1baa35f
qweqwe1qq <dfofxadze2010@gmail.com>
2024-02-09 23:34:27 +0400

    homework3

---
+++ day 3/homework/day3.py
@@ -0,0 +1,10 @@
+dad = "zviadi popkhadze,"
+mom = "nini lapiashvili-popkhadze,"
+brother = "luka popkhadze,"
+older_sister = "tatuli popkhadze,"
+younger_sister = "nensi popkhadze,"
+grandmom = "juli popkhadze,"
+granddad = "tamazi popkhadze."
+
+
+print(dad + " " + mom + " " + brother + " " + older_sister + " " + younger_sister + " " + grandmom + " " + granddad)
