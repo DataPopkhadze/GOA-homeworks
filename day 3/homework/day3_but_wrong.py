a275db230c9f8053386afe5be0a280125056be38
qweqwe1qq <dfofxadze2010@gmail.com>
2024-02-09 23:43:39 +0400

    commit

---
+++ day 3/homework/day3_but_wrong.py
@@ -0,0 +1,10 @@
+d a d =      "zviadi popkhadze,"
+m om  = "nini la  piashvili-popkhadze,"
+b ro th e r = "luka popkhadze,"
+older _sister = "tatuli popkhadze,"
+young er_s is ter = "nensi popkhadze,"
+gra nd m om = "juli popkhadze,"
+gran d da d = "tamazi popkhadze."
+
+
+p r i n t(  da d + " " + mo  m + " " + brother + " " + older_sister + " " + younger_sister + " " + grandmom + " " + granddad)
