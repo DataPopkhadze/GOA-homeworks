8f3d72b41e43c7ae3130fc0417cd51e9dcf170dc
qweqwe1qq <dfofxadze2010@gmail.com>
2024-02-13 19:40:41 +0400

    commit

---
+++ day 04/classwork/day04.py
@@ -0,0 +1,10 @@
+my_name = "data"
+#my_name aris cvladis saxeli
+#toloba aris mimniwebeli operatori
+#data aris cvladis mnishvneloba (variable value)
+print(my_name)
+
+x = 5
+y = 10
+
+print(x + y)
---
+++ day 04/homework/day04.py
@@ -0,0 +1,5 @@
+equation = (15 * 20)
+numbers = (320 / 5)
+
+print(equation)
+print(numbers)
