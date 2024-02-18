c0f35a25b8f75f5492c911a94ca20e2e57e70dd8
qweqwe1qq <dfofxadze2010@gmail.com>
2024-02-18 14:05:19 +0400

    commit

---
+++ day 05/classwork/day05.py
@@ -0,0 +1,12 @@
+# my_name = "data"
+# #in python undesrcore's name is snakecase
+
+# MyName = "data"
+# #in python this kind of text is called camelcase
+
+# print(my_name)
+
+salary = 50000
+role = Analyst
+age -> 29
+print(salary)
---
+++ day 05/homework/day05.py
@@ -0,0 +1,5 @@
+number1 = (500 * 4 + 190)
+
+number2 = (300 / 2 - 56)
+
+print(number1 + number2)
