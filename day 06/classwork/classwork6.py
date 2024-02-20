f70c8f30cdbc86e8993dd93ea244d1855816e85e
qweqwe1qq <dfofxadze2010@gmail.com>
2024-02-20 20:16:22 +0400

    commit

---
+++ day 06/classwork/classwork6.py
@@ -0,0 +1,15 @@
+# name = input("please enter your name: ")
+# surname = input("please enter your lastname: ")
+# age = input("please enter your age: ")
+
+# print(name , surname , age)
+
+
+
+
+
+num1 = int(input("please enter yout number 1: "))
+num2 = int(input("please enter your number 2: "))
+num3 = int(input("please enter your number 3: "))
+
+print(num1 + num2 + num3)
