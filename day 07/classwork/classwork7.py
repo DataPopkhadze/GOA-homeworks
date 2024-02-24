397e75685afa8aaf86e96f6988921e0d7ffa3a5e
qweqwe1qq <dfofxadze2010@gmail.com>
2024-02-24 23:26:00 +0400

    commit

---
+++ day 07/classwork/classwork7.py
@@ -0,0 +1,12 @@
+# name = input("please enter your name: ")
+# surname = input("please enter your surname: ")
+# age = int(input("please enter your age: "))
+
+# print(name, surname, age)
+
+
+num1 = int(input("please enter your number: "))
+num2 = int(input("please enter your number: "))
+num3 = int(input("please enter your number: "))
+
+print(num1 > num2 == num3)     
